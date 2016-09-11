c1869cdffc5d9f6a24f5c1b23a67fa57b9334a5c
Vinay <vgvinay2@gmail.com>
2016-09-12 00:38:18 +0530

    all small changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,12 @@ module ApplicationHelper
 	def get_rank_data
 		Rank.pluck("DATE(created_at)")
 	end
+
+	def get_global_rank site
+		site.ranks.pluck(:global_rank).join(',') rescue 0
+	end
+
+	def get_local_rank site
+	  site.ranks.pluck(:local_rank).join(',')  rescue 0
+	end	
 end
