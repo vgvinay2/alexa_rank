5f61b800e71f9889bd0d1baf57291af039f79be7
Vinay <vgvinay2@gmail.com>
2016-09-13 17:37:46 +0530

    change in strip

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,8 +8,8 @@ require 'open-uri'
 	    doc = Nokogiri::HTML(open(url))
 	    if doc.css('strong.metrics-data.align-vmiddle').present? 
         current_site_url = current_user.site_urls.find_or_create_by(site_name: params[:search])
-	      global_rank = { global_rank: doc.css('strong.metrics-data.align-vmiddle').children[2].text.strip.to_i }
-	      local_rank = { local_rank:   doc.css('strong.metrics-data.align-vmiddle').children[3].text.strip.to_i }
+	      global_rank = { global_rank: doc.css('strong.metrics-data.align-vmiddle').children[2].text.strip }
+	      local_rank = { local_rank:   doc.css('strong.metrics-data.align-vmiddle').children[3].text.strip }
 	      rank = global_rank .merge(local_rank)
         current_site_url.ranks.create rank if current_site_url.ranks.exists_rank.blank? 
       else
